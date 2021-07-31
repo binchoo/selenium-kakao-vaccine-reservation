a0f205a6b5a4214ce3fe1c5569c2a566788aa4fa
binchoo <079111w@gmail.com>
2021-07-31 13:30:13 +0900

    Add on_error lifecycle

--- service/lifecycle.py
+++ service/lifecycle.py
@@ -9,9 +9,15 @@ class LifeCycleMixin:
     def on_progress(self, func):
         self.on_progress_listener = func
 
+    def on_error(self, func):
+        self.on_progress_listener =
+
     def start(self):
         self.on_start_listener(self)
-        self._start()
+        try:
+            self._start()
+        except Exception as e:
+            self.on_error_listener(self, e)
         self.on_end_listener(self)
 
     def _start(self):
