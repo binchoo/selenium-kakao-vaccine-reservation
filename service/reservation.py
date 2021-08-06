7b348e4f62be348bcd30cefaae2b2c88ef0be48a
binchoo <079111w@gmail.com>
2021-08-06 14:09:23 +0900

    Do not register Reservation service to global context.

--- service/reservation.py
+++ service/reservation.py
@@ -6,42 +6,21 @@ from service.lifecycle import LifeCycleMixin
 
 class LegacyVaccineReservation(LifeCycleMixin):
 
-    def __init__(self, login_cookie, region, interval=7):
+    def __init__(self):
         super().__init__()
-        self.login_cookie = login_cookie
-        self.region = region
-        self.search_time = interval
-        self.header = constant.header.get('kakao')
         self.reservation_url = constant.url.get('kakao').get('reservation')
-        self._kill = False
+        self.header = constant.header.get('kakao')
         self.vaccine_type = "ANY"
         self.view_logger = None
-
-    def start(self):
-        self.on_start_listener(self)
-        self._start()
-        self.on_end_listener(self)
-
-    def vaccine_type_input(self): #TODO: 미아 코드 제거
-        vaccine_type = None
-        while vaccine_type is None:
-            print("""
-                <백신 코드 일람>
-                화이자         : VEN00013
-                모더나         : VEN00014
-                아스크라제네카   : VEN00015
-                얀센          : VEN00016
-                아무거나       : ANY
-            """)
-            vaccine_type = str.upper(input("예약시도할 백신 코드를 알려주세요."))
-        return vaccine_type
+        self._found = self._kill = False
 
     def _start(self):
-        found = False
-        while not found and not self._kill: 
-            vaccine_remaining = self.find_vaccine_remaining("ANY") #TODO: 백신 타입 값 인젝션 추가하기
+        self._validate_dependencies()
+        self._set_flags(found=False, kill=False)
+        while not self._found and not self._kill: 
+            vaccine_remaining = self.find_vaccine_remaining(self.vaccine_type)
             if vaccine_remaining is None:
-                time.sleep(self.search_time)
+                time.sleep(self.run_interval)
             else:
                 print(f"주소는 : {vaccine_remaining.get('address')} 입니다.")
                 organization_code = vaccine_remaining.get('orgCode')
@@ -53,20 +32,21 @@ class LegacyVaccineReservation(LifeCycleMixin):
                 
                 for vaccine_type in try_vaccine_types:
                     if self.try_reservation(organization_code, vaccine_type):
-                        found = True
-                        self._kill = True
+                        self._set_flags(found=True, kill=True)
                         break
+
+    def _validate_dependencies(self):
+        if not all(
+            [hasattr(self, prop) for prop in ['login_cookie', 'region', 'run_interval', 'vaccine_type']]
+        ):
+            raise RuntimeError('필요한 의존성이 전달되지 않았습니다.')
+
+    def _set_flags(self, found, kill):
+        self._found = found
+        self._kill = kill
 
     def set_view_logger(self, qtwidget):
         self.view_logger = qtwidget
-
-    def _print(self, msg):
-        if self.view_logger is not None:
-            self.view_logger.log(msg)
-        print(msg)
-
-    def print_to_qwidget(self, msg, qwidget):
-        qwidget.append(msg)
 
     def find_vaccine_remaining(self, vaccine_type):
         left_by_coords_url = constant.url.get('kakao').get('left_by_coords')
@@ -81,7 +61,7 @@ class LegacyVaccineReservation(LifeCycleMixin):
                 print('Response Error Occurred.')
                 raise requests.exceptions.ConnectionError
 
-            self.pretty_print(response_json)
+            self._pretty_print(response_json)
             for x in response_json.get("organizations"):
                 if x.get("status") == "AVAILABLE" or x.get("leftCounts") != 0:
                     vaccine_remaining = x
@@ -104,6 +84,19 @@ class LegacyVaccineReservation(LifeCycleMixin):
 
         return vaccine_remaining
 
+    def _pretty_print(self, response_json):
+        for org in response_json["organizations"]:
+            if org.get('status') == "CLOSED" or org.get('status') == "EXHAUSTED":
+                continue
+            print(
+                f"잔여갯수: {org.get('leftCounts')}\t상태: {org.get('status')}\t기관명: {org.get('orgName')}\t주소: {org.get('address')}")
+        self._print(str(datetime.now()))
+
+    def _print(self, msg):
+        if self.view_logger is not None:
+            self.view_logger.log(msg)
+        print(msg)
+
     def try_reservation(self, organization_code, vaccine_type, try_loops=2):
         data = {
             "from": "Map", 
@@ -121,7 +114,7 @@ class LegacyVaccineReservation(LifeCycleMixin):
             response_json = json.loads(response.text)
             print(response_json)
 
-            if response.status_code != 200:
+            if response.status_code == 500:
                 print('Response Error Occurred.')
 
             for key in response_json:
@@ -141,14 +134,6 @@ class LegacyVaccineReservation(LifeCycleMixin):
                     self._print("ERROR. 아래 메시지를 보고, 예약이 신청된 병원 또는 1339에 예약이 되었는지 확인해보세요.")
                     self._print(response.text)
         return False
-
-    def pretty_print(self, response_json):
-        for org in response_json["organizations"]:
-            if org.get('status') == "CLOSED" or org.get('status') == "EXHAUSTED":
-                continue
-            print(
-                f"잔여갯수: {org.get('leftCounts')}\t상태: {org.get('status')}\t기관명: {org.get('orgName')}\t주소: {org.get('address')}")
-        self._print(str(datetime.now()))
 
     def interrupt(self):
         self._kill = True
